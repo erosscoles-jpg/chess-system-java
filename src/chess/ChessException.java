d1eb64142ca4690e79efeb1ae99be978ab9c1dcc
Eros Scoles <eros.scoles@gmail.com>
2026-07-16 15:27:39 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,6 +1,8 @@
 package chess;
 
-public class ChessException extends RuntimeException{
+import Boardgame.BoardExeception;
+
+public class ChessException extends BoardExeception{
 
 	private static final long serialVersionUID = 1L;
 
